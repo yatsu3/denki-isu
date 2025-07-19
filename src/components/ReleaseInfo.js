fc4b26adaa3132e0ad2c39b427baaadb4587d96c
yatsu3 <yatsutako3gmail.com>
2025-07-19 12:13:51 +0900

    リリースページ修正

--- src/components/ReleaseInfo.js
+++ src/components/ReleaseInfo.js
@@ -8,6 +8,7 @@ const ReleaseInfo = () => {
       <ul style={{ color: '#444', fontSize: '1.05rem', lineHeight: 1.7, marginBottom: 32 }}>
         <li>2024-07-01: リリース開始</li>
         <li>2024-07-11: リリースページ作成</li>
+        <li>2025-07-19: ファビコン設定</li>
       </ul>
       <div style={{ textAlign: 'right' }}>
         <Link to="/" style={{ color: '#007bff', textDecoration: 'underline', fontSize: '1rem' }}>トップページに戻る</Link>
